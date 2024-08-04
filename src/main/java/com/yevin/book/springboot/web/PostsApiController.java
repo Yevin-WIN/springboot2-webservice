775e02fe29fc4cca951117736e0c65ea310a0573
김예빈 <kimyevin17@gmail.com>
2024-08-05 00:31:04 +0900

    Ch.3 JPA Database

---
+++ src/main/java/com/yevin/book/springboot/web/PostsApiController.java
@@ -0,0 +1,30 @@
+package com.yevin.book.springboot.web;
+
+
+import com.yevin.book.springboot.service.PostsService;
+import com.yevin.book.springboot.web.dto.PostsResponseDto;
+import com.yevin.book.springboot.web.dto.PostsSaveRequestDto;
+import com.yevin.book.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+    public final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id){
+        return postsService.findById(id);
+    }
+}
